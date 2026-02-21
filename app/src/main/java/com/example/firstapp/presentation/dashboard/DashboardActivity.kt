2756b04169de4f27dd28bb1f3c2a063f902b35a0
kalvin-osoro <kalvinosoro@gmail.com>
2026-02-21 07:43:21 +0300

    set up jetpack compose clean architecture

---
+++ app/src/main/java/com/example/firstapp/presentation/dashboard/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firstapp.presentation.dashboard
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class DashboardActivity : ComponentActivity() {
+
+
+}
---
+++ app/src/main/java/com/example/firstapp/presentation/dashboard/screens/home/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.example.firstapp.presentation.dashboard.screens.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun HomeScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+
+
+
+) {}
