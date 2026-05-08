38c01a733697234cc78b9fc5577c76ff69ec13ad
kalvin-osoro <kalvinosoro@gmail.com>
2026-05-08 23:57:34 +0300

    cleanup

--- app/src/main/java/com/example/firstapp/data/repository/SpotifyRepositoryImpl.kt
+++ app/src/main/java/com/example/firstapp/data/repository/SpotifyRepositoryImpl.kt
@@ -4,12 +4,11 @@ import com.example.firstapp.data.spotify.SpotifyAuthManager
 import com.example.firstapp.domain.repository.SpotifyRepositoryInterface
 import javax.inject.Inject
 
-class SpotifyRepositoryImpl @Inject constructor(
+class bSpotifyRepositoryImpl @Inject constructor(
     private val authManager: SpotifyAuthManager
 ) : SpotifyRepositoryInterface {
     override suspend fun login(): Result<String> {
         return try {
-            // This will actually be triggered from Activity
             Result.success("Login started")
         } catch (e: Exception) {
             Result.failure(e)
